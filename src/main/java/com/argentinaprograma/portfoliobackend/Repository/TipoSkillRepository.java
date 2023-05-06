1dfddeff883bcb018e839817db268cc7be27d3d6
Gonzalo <gonzamaunas@gmail.com>
2023-05-06 20:30:48 -0300

    Agregados Repositorios

---
+++ src/main/java/com/argentinaprograma/portfoliobackend/Repository/TipoSkillRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.argentinaprograma.portfoliobackend.Repository;
+
+import com.argentinaprograma.portfoliobackend.Model.TipoSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author gonza
+ */
+public interface TipoSkillRepository extends JpaRepository<TipoSkill, Long> {
+    
+}
