f574bc04d4d95646455e1d8e8c36dc9db0ead131
Gonzalo <gonzamaunas@gmail.com>
2023-05-11 15:01:43 -0300

    Errores en el Controller corregidos

--- src/main/java/com/argentinaprograma/portfoliobackend/Controller/PortfolioController.java
+++ src/main/java/com/argentinaprograma/portfoliobackend/Controller/PortfolioController.java
@@ -7,6 +7,7 @@ package com.argentinaprograma.portfoliobackend.Controller;
 import com.argentinaprograma.portfoliobackend.Dto.*;
 import com.argentinaprograma.portfoliobackend.Service.PortfolioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author gonza
  */
+@CrossOrigin
 @RestController
 public class PortfolioController {
 
